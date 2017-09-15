302014a9446e6aeee4dfee3f11637ece101b48d6
Eric Roginek <eroginek@protonmail.com>
2017-09-15 17:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
-<<<<<<< HEAD
 console.log(message)
-=======
-console.log(message)
->>>>>>> 7ca763554cac202f5b04d5f2156fcde564dfb3fa
